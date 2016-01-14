e0edcb85278ef5d2cd7320c6e32008c0bdd2eecb
Rajat Sud <rajatsud@yahoo.com>
2016-01-14 14:58:20 -0500

    Driver Configuration

--- src/com/totalwine/test/trials/Browser.java
+++ src/com/totalwine/test/trials/Browser.java
@@ -12,22 +12,25 @@ package com.totalwine.test.trials;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -39,10 +42,10 @@ import com.totalwine.test.config.ConfigurationFunctions;
 public class Browser {
 
 	protected WebDriver driver;
-	
+	protected String hubURL = "http://prt-6rkhd12.totalwine.com:5566/wd/hub";
 	
 	@BeforeMethod
-	//
+	
 	@Parameters("browser") 
 	public void openBrowser(String browser) {
 		//Firefox
@@ -59,7 +62,6 @@ public class Browser {
 		}
 		//IE
 		if (browser.equalsIgnoreCase("IE")) {
-			//File file = new File("C:/totalwine/Library/IEDriverServer.exe");
 			File file = new File(ConfigurationFunctions.IEDRIVERPATH);
 			System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
 			DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
@@ -80,14 +82,12 @@ public class Browser {
 		}
 		//Chrome
 		if (browser.equalsIgnoreCase("Chrome")) {
-			//File file = new File("C:/totalwine/Library/chromedriver.exe");
 			File file = new File(ConfigurationFunctions.CHROMEDRIVERPATH);
 			System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 			driver = new ChromeDriver();
 		}
 		//iOS (iPhone 6)
 		if (browser.equalsIgnoreCase("iOS")) {
-			//File file = new File("C:/totalwine/Library/chromedriver.exe");
 			File file = new File(ConfigurationFunctions.CHROMEDRIVERPATH);
 			System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 			Map<String, String> mobileEmulation = new HashMap<String, String>();
@@ -100,7 +100,6 @@ public class Browser {
 		}
 		//Android (Samsung Galaxy S4)
 		if (browser.equalsIgnoreCase("Android")) {
-			//File file = new File("C:/totalwine/Library/chromedriver.exe");
 			File file = new File(ConfigurationFunctions.CHROMEDRIVERPATH);
 			System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 			Map<String, String> mobileEmulation = new HashMap<String, String>();
@@ -111,33 +110,67 @@ public class Browser {
 			capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
 			driver = new ChromeDriver(capabilities);
 		}
-		//Android (Samsung Galaxy S4)
-		if (browser.equalsIgnoreCase("CUA")) {
-			//File file = new File("C:/totalwine/Library/chromedriver.exe");
-			File file = new File(ConfigurationFunctions.CHROMEDRIVERPATH);
-			System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-			Map<String, String> mobileEmulation = new HashMap<String, String>();
-			mobileEmulation.put("userAgent", "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19");
-			Map<String, Object> chromeOptions = new HashMap<String, Object>();
-			chromeOptions.put("mobileEmulation", mobileEmulation);
-			DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-			capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-			driver = new ChromeDriver(capabilities);
-		}
 		//Headless
 		if (browser.equalsIgnoreCase("Headless")) {
-			// Declaring and initializing the HtmlUnitWebDriver
+			// Declaring and initialising the HtmlUnitWebDriver
 			HtmlUnitDriver driver = new HtmlUnitDriver();
 		}
+		//Grid - Firefox
+		if(browser.equalsIgnoreCase("GridFF")) {
+			DesiredCapabilities cap = DesiredCapabilities.firefox();
+		    cap.setBrowserName("firefox");
+		    cap.setPlatform(Platform.VISTA);
+		    try {
+				driver = new RemoteWebDriver(new URL(hubURL),cap); //Hub URL
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} 
+		}
+		//Grid - Chrome
+		if(browser.equalsIgnoreCase("GridChrome")) {
+			DesiredCapabilities cap = DesiredCapabilities.chrome();
+		    cap.setBrowserName("chrome");
+		    cap.setPlatform(Platform.VISTA);
+		    try {
+				driver = new RemoteWebDriver(new URL(hubURL),cap); //Hub URL
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} 
+		}
+		//Grid - IE
+		if(browser.equalsIgnoreCase("GridIE")) {
+			DesiredCapabilities cap = DesiredCapabilities.internetExplorer();
+		    cap.setBrowserName("internet explorer");
+		    cap.setPlatform(Platform.VISTA);
+		    try {
+				driver = new RemoteWebDriver(new URL(hubURL),cap); //Hub URL
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+		}
+		//Grid - iOS (iPhone 6)
+				if (browser.equalsIgnoreCase("iOSGrid")) {
+					DesiredCapabilities cap = DesiredCapabilities.chrome();
+					Map<String, String> mobileEmulation = new HashMap<String, String>();
+					mobileEmulation.put("deviceName", "Apple iPhone 6");
+					Map<String, Object> chromeOptions = new HashMap<String, Object>();
+					chromeOptions.put("mobileEmulation", mobileEmulation);
+					cap.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+				    cap.setBrowserName("chrome");
+				    cap.setPlatform(Platform.VISTA);
+				    try {
+						driver = new RemoteWebDriver(new URL(hubURL),cap); //Hub URL
+					} catch (MalformedURLException e) {
+						e.printStackTrace();
+					} 
+				}
 	}
 	
 	@AfterMethod
 	public void takeScreenShotOnFailure(ITestResult testResult) throws IOException, InterruptedException { 
 		if(testResult.getStatus() == ITestResult.FAILURE) { 
 			File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-			//FileUtils.copyFile(scrFile, new File("c:\\FailureScreenshots\\FAIL "+testResult.getName()+"  "+ConfigurationFunctions.now()+".png")); 
 			FileUtils.copyFile(scrFile, new File("C:\\Users\\rsud\\.jenkins\\userContent\\FailureScreenshots\\UAT\\FAIL "+testResult.getName()+"  "+ConfigurationFunctions.now()+".png")); 
-			
 		}
 		driver.close();
 	}
